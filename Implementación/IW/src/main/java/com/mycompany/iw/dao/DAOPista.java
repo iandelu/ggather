962156dd3006a5986b2150227770e82184e9dcda
Youness Bettahi Berdai <48675571+yunesss00@users.noreply.github.com>
2021-05-08 01:03:39 +0200

    Create DAOPista.java

---
+++ Implementación/IW/src/main/java/com/mycompany/iw/dao/DAOPista.java
@@ -0,0 +1,15 @@
+package com.mycompany.iw.dao;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author youne
+ */
+public class DAOPista {
+    
+}

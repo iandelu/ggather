13987d3f871dd768a0e3e27b56103bf68081a211
Aneri00 <48674592+i82andel@users.noreply.github.com>
2021-05-08 01:47:20 +0200

    mejora de clase 1.47

--- Implementación/IW/src/main/java/com/mycompany/iw/Puntuacion.java
+++ Implementación/IW/src/main/java/com/mycompany/iw/Puntuacion.java
@@ -9,6 +9,55 @@ package com.mycompany.iw;
  *
  * @author LuisAneri
  */
+
 public class Puntuacion {
+    
+    private Long id = null;
+    private int puntuacion;
+    private String comentario;
+    private Pista pista;
+
+    public Puntuacion(int puntuacion, String comentario, Pista pista) {
+        this.puntuacion = puntuacion;
+        this.comentario = comentario;
+        this.pista = pista;
+    }
+
+    //getter puntuacion
+    public Long getId() {
+        return id;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public Pista getPista() {
+        return pista;
+    }
+    
+    
+    
+    //setter puntacion
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setPista(Pista pista) {
+        this.pista = pista;
+    }
     
 }

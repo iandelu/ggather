d7a61c2d2283be45757d1e30e53a8e790a15f67c
Cristian1711 <i82goorc@uco.es>
2021-05-08 14:08:01 +0200

    version actualizada DAOJugador (no lo toqueis mas anda)

--- Implementación/IW/src/main/java/com/mycompany/iw/dao/DAOJugador.java
+++ Implementación/IW/src/main/java/com/mycompany/iw/dao/DAOJugador.java
@@ -50,10 +50,11 @@ public class DAOJugador extends ConexionBD{
             ps.setString(2, jugador.getContraseña());
             ps.setString(3, jugador.getEmail());
             ps.setDate(4, jugador.getFechaNacimiento());
-            ps.setLong(5, jugador.getId());
             ps.setString(6, jugador.getNombre());
             ps.setInt(7, jugador.getTelefono());
             ps.setString(8, jugador.getUsuario());
+            
+            status = ps.executeUpdate();
             
         } catch (Exception e){System.out.println(e);}
         
@@ -67,10 +68,17 @@ public class DAOJugador extends ConexionBD{
         
         try {
             
+            Connection con = getConnection(url, usuario, password);
+            PreparedStatement ps = con.prepareStatement(sqlProperties.getProperty("modificarJugador"));
+            ps.setString(1, jugador.getApellidos());
+            ps.setString(2, jugador.getEmail());
+            ps.setDate(3, jugador.getFechaNacimiento());
+            ps.setString(4, jugador.getUsuario());
+            ps.setInt(5, jugador.getTelefono());
+            ps.setString(6, jugador.getUsuario());
+            ps.setString(7, jugador.getUsuario());
             
-            
-            
-            
+            status = ps.executeUpdate();
             
         } catch(Exception e){ System.out.println(e);}
         
@@ -79,12 +87,53 @@ public class DAOJugador extends ConexionBD{
     }
     
     
+    public int borrarJugador(Jugador jugador){
+        
+        int status = 0;
+        
+        
+        try {
+           
+            Connection con = getConnection(url, usuario, password);
+            PreparedStatement ps = con.prepareStatement(sqlProperties.getProperty("borrarJugador"));
+            ps.setString(1, jugador.getUsuario());
+            
+            status = ps.executeUpdate();
+            
+           
+        } catch(Exception e) { System.out.println(e);}
+    
+        return status;
+    }
     
     
-    
-    
-    
-    
+    public Jugador buscarJugador(Jugador jugador){
+        
+        PreparedStatement stmt = null;
+        Jugador resul = null;
+        ResultSet rs = null;
+        
+        try {
+            
+            Connection con = getConnection(url, usuario, password);
+            stmt = con.prepareStatement(sqlProperties.getProperty("buscarJugador"));
+            stmt.setString(1, jugador.getUsuario());
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                
+                resul = new Jugador(rs.getString("usuario"), rs.getString("nombre"), rs.getString("apellidos"), rs.getString("email"), rs.getInt("telefono"), rs.getInt("nivel"), rs.getString("deporte"));             
+            }
+            
+                   
+        } catch(Exception e) {System.out.println(e);}
+        
+        
+        return resul;
+        
+        
+        
+    }
     
     
     

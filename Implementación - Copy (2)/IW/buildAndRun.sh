3ca1a5bb88a989e2cbc82783d2371975613c5b4b
Aneri00 <48674592+i82andel@users.noreply.github.com>
2021-05-21 12:29:32 +0200

    solucionar copia

--- Implementación - Copy (2)/IW/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.mycompany/IW .
-docker rm -f IW || true && docker run -d -p 9080:9080 -p 9443:9443 --name IW com.mycompany/IW
--- Implementación - Copy/IW/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/IW .
-call docker rm -f IW
-call docker run -d -p 9080:9080 -p 9443:9443 --name IW com.mycompany/IW
